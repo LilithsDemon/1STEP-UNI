e9d734bf83cb6bf4960ec4922fce071a90afd407
MiniMECHlg <lucy.gardener2004@gmail.com>
2022-10-29 17:55:30 +0100

    Simple starting

---
+++ main.js
@@ -0,0 +1,20 @@
+const hamburger = document.querySelector('.hamburger');
+const mobile_menu_items = document.querySelectorAll('.m_m_item');
+const mobile_menu = document.querySelector('.mobile-menu');
+
+function toggleMenu ()
+{
+    hamburger.classList.toggle('is-active');
+    mobile_menu.classList.toggle('is-open');
+}
+
+hamburger.addEventListener('click', function() {
+    toggleMenu();
+});
+
+for(var i = 0; i < mobile_menu_items.length; i++)
+{
+    mobile_menu_items[i].addEventListener('click', function(){
+        toggleMenu();
+    });
+}
